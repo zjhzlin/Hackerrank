ffe1711dbcdd7592720ef968414fe806e8547f0b
zjhzlin <74384600+zjhzlin@users.noreply.github.com>
2021-04-03 15:22:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+#
+import math
+import os
+import random
+import re
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    n = 1
+    if n % 2 == 1:
+        print('Wierd')
+    elif n % 2 == 0:
+        print('Not Wierd')
